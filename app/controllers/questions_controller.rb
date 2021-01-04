a570872124ceed878bf6d31e24a5221b0ff76370
matthewxvoorhees <matthew.crnich@gmail.com>
2021-01-03 19:42:24 -0700

    Messing with routes to fix Post issue

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -46,6 +46,6 @@ class QuestionsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def question_params
-      params.require(:question).permit(:body, :answer)
+      params.require(:question).permit(:category_id, :body, :answer)
     end
 end
